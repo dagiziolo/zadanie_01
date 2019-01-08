7843914c219dbd7cbf7b7d5fcccbc84d0208c3a2
Dagmara Szczepańska <dagmara.ziolkowska@wp.pl>
2019-01-08 22:39:25 +0100

    Dodanie dodatkowego pliku - celem poćwiczenia

---
+++ src/ChristmasTree.java
@@ -0,0 +1,15 @@
+public class ChristmasTree {
+    public static void main(String[] args) {
+        System.out.println("Chciałam potrenować umieszczanie nowych plików,");
+        System.out.println("a choinka mi się spodobała jako przykład.\n"); //celowo dodatkowe przejście do lini
+        System.out.println("     *");
+        System.out.println("    ***");
+        System.out.println("   *****");
+        System.out.println("  *******");
+        System.out.println("     *");
+        System.out.println("     *\n");
+
+        System.out.println("Pewnie można lepiej pętlą - ale to później, jak więcej nauczę się programowania ;)");
+
+    }
+}
